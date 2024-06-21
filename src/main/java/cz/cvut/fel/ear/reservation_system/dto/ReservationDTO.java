34b14bc1e3dafe6aaf50a69dfdbfe38c9d273708
Pavel Smiga <smigapav@fel.cvut.cz>
2024-06-21 09:58:12 +0200

    fix: Mapper fixes

--- src/main/java/cz/cvut/fel/ear/reservation_system/dto/ReservationDTO.java
+++ src/main/java/cz/cvut/fel/ear/reservation_system/dto/ReservationDTO.java
@@ -1,39 +1,38 @@
 package cz.cvut.fel.ear.reservation_system.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import cz.cvut.fel.ear.reservation_system.model.Reservation;
+import cz.cvut.fel.ear.reservation_system.model.ReservationStatus;
+import cz.cvut.fel.ear.reservation_system.model.Room;
+import cz.cvut.fel.ear.reservation_system.model.User;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 public class ReservationDTO {
     @JsonProperty("id")
-    private int id;
+    private Integer id;
+
+    @JsonIgnore
+    private User user;
+
+    @JsonIgnore
+    private Room room;
+
     @JsonProperty("dateFrom")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime dateFrom;
+
     @JsonProperty("dateTo")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime dateTo;
-    @JsonProperty("createdAt")
-    private LocalDateTime createdAt;
+
     @JsonProperty("status")
-    private String status;
-
-    @JsonProperty("userId")
-    private int userId;
-
-    @JsonProperty("userName")
-    private String userName;
-
-    @JsonProperty("roomId")
-    private int roomId;
-
-    @JsonProperty("roomName")
-    private String roomName;
-
-    @JsonProperty("confirmedAt")
-    private LocalDateTime confirmedAt;
-
-    @JsonProperty("totalPrice")
-    private Double totalPrice;
+    private ReservationStatus status;
 
     public ReservationDTO() {
     }
